55a177832e4b00e97448a3a7a13be7ed4932650e
safalyakumbhare <safalyakumbhare@gmail.com>
2023-09-20 00:16:53 +0530

    Password Manager

--- Password Manager/script.js
+++ Password Manager/script.js
@@ -1,1 +1,6 @@
-
+console.log("Working");
+document.querySelector("button").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("CLicked");
+  console.log(username.value, password.value);
+});
